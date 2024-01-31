c4260ecc652f9afe4e5f0504158925c3efe0e7b8
tcaa-web <kfry@techcollege.dk>
2024-01-31 13:19:35 +0100

    Camp page added

--- src/pages/Camppage/Camppage.jsx
+++ src/pages/Camppage/Camppage.jsx
@@ -1,3 +1,42 @@
+import { NewsCard } from "../../components/NewsCard/NewsCard";
+import { Title } from "../../components/Title/Title";
+import { useFetch } from "../../hooks/useFetch";
+import style from "./Camppage.module.scss";
+
 export const Camppage = () => {
-  return <div>Camp page</div>;
+  const camps = useFetch("https://api.mediehuset.net/mediesuset/camps");
+
+  console.log(camps);
+
+  return (
+    <>
+      <Title title={"Camps"} />
+      <section className={style.campWrapper}>
+        {camps?.items.map((item) => {
+          const textContent = (
+            <>
+              <p>
+                {item.name} er en camp med plads til {item.num_people}. Følgende
+                armbånd giver adgang til denne camp:
+              </p>
+              <br />
+              <ul>
+                {item.tickets.map((item) => {
+                  return <li>{item.name + " - " + item.price + " DKK"}</li>;
+                })}
+              </ul>
+            </>
+          );
+
+          return (
+            <NewsCard
+              title={item.name}
+              imgSrc={item.image}
+              text={textContent}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 };
