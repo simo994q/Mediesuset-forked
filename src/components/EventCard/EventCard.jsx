529ce8bcbda32de9ad11cb1914a4c7fc0e3ecff4
kasperfryd <kfry@techcollege.dk>
2024-02-01 14:32:08 +0100

    added infopage, add programme func & react-icons dep

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,15 +1,50 @@
 import style from "../EventCard/EventCard.module.scss";
+import { CiCirclePlus } from "react-icons/ci";
 
-export const EventCard = ({ image, title, date, stageColor, setEventID }) => {
+export const EventCard = ({
+  image,
+  title,
+  date,
+  stageColor,
+  setEventID,
+  userID,
+  eventID,
+  token,
+}) => {
+  const addToUserProgram = () => {
+    let url = "https://api.mediehuset.net/mediesuset/programme";
+    let body = new URLSearchParams();
+    body.append("user_id", userID);
+    body.append("event_id", parseInt(eventID));
+
+    let options = {
+      body: body,
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <figure onClick={setEventID} className={style.eventCardStyle}>
-      <div>
+    <figure className={style.eventCardStyle}>
+      <div onClick={setEventID}>
         <img src={image} />
       </div>
       <figcaption style={{ backgroundColor: stageColor }}>
         <p>{title}</p>
         <p>{date}</p>
-        <p>Læs mere</p>
+        <p onClick={setEventID}>Læs mere</p>
+        {userID && (
+          <span className={style.plusIcon} onClick={() => addToUserProgram()}>
+            <CiCirclePlus />
+          </span>
+        )}
       </figcaption>
     </figure>
   );
